69f926b2bfd9132e3bdfa676b2f8f5456881de80
emmanuel ronoh <eronoh036@gmail.com>
2024-08-02 17:21:24 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,68 @@
+// let employee1 = "Gilbert";
+// let employee2 = "dismuss";
+// let employee3 = "chebet";
+// let employee4 = "geofry";
+// let employee5 = "enock";
+// let employee6 = "emmanuel";
+// let employee7 = "victor";
+// let employee8 = "larry";
+// let employee9 = "evans";
+// let employee10 = "shalyne";
+// let employee11 = "osir"
+// console.log(
+//     employee1,
+//     employee2, 
+//     employee3, 
+//     employee4, 
+//     employee5, 
+//     employee6, 
+//     employee7, 
+//     employee8, 
+//     employee9, 
+//     employee10)
+
+let employees = ["gilbert", "osir", "shalyne", "snappy", "evans", "josephine", "sharon", "aleph", "phope", "leonard"];
+// console.log(employees[0]);
+// console.log(employees[1]);
+// console.log(employees[2]);
+
+// let employees = [];
+// employees.push("gilbert");
+// console.log(employees);
+// employees.push("osir");
+// console.log(employees);
+// employees.push("shalyne");
+// console.log(employees);
+// employees.pop();
+// console.log(employees);
+// employees.unshift("shalyne");
+// console.log(employees);
+// employees.shift();
+// console.log(employees);
+// employees.splice(1,2, "muktar", "maureen" );
+// console.log(employees);
+// employees.splice(8,3, "mowlid", "joseph","otieno" );
+
+// let employees1 = employees.slice(3, 8);
+// console.log(employees.length);
+// console.log(employees1);
+
+let numberOfEmployees = function () {
+    // for (
+    //     let employeeNumber = 0; 
+    //     employeeNumber < employees.length;
+    //     employeeNumber = employeeNumber + 1
+    // ) {
+    //    console.log(employees[employeeNumber]);
+    // }
+    
+};
+// console.log(numberOfEmployees());
+
+
+let newFunctionNumbers = function () {
+    for(let i = 0; i < employees.length; i++) {
+        console.log(employees[i]);
+    }
+    }
+console.log(newFunctionNumbers());
